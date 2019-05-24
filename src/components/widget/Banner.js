7e9fedec5580081ba33213fa3c025fb6ecbb5e75
Django Cass <dj.cass44@gmail.com>
2019-05-24 22:59:03 +1000

    Added dismiss button to banner

--- src/components/widget/Banner.js
+++ src/components/widget/Banner.js
@@ -20,16 +20,35 @@ import Card from "@material-ui/core/es/Card/Card";
 import Avatar from "@material-ui/core/es/Avatar/Avatar";
 import ListItemText from "@material-ui/core/es/ListItemText";
 import ListItem from "@material-ui/core/ListItem";
+import ListItemSecondaryAction from "@material-ui/core/es/ListItemSecondaryAction/ListItemSecondaryAction";
+import IconButton from "@material-ui/core/IconButton";
+import ClearIcon from "@material-ui/icons/ClearOutlined";
 
-class Banner extends React.Component{
+class Banner extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			visible: true
+		};
+	}
+
 	render() {
 		return <Card style={{borderRadius: 12, marginBottom: 8}}>
-			{this.props.open === true ?
-				<ListItem button disableRipple key={this.props.label}>
+			{this.props.open === true && this.state.visible === true ?
+				<ListItem key={this.props.label}>
 					<Avatar style={this.props.avatarStyle}>
 						{this.props.icon}
 					</Avatar>
 					<ListItemText primary={<span>{this.props.label}</span>}/>
+					{this.props.showDismissButton === true ?
+						<ListItemSecondaryAction>
+							<IconButton size={'small'} onClick={() => this.setState({visible: false})}>
+								<ClearIcon/>
+							</IconButton>
+						</ListItemSecondaryAction>
+						:
+						""
+					}
 				</ListItem>
 				:
 				<div/>
