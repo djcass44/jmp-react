049d80397742c5d640c387b4aa91a1fa9135031e
Django Cass <dj.cass44@gmail.com>
2019-08-13 22:28:34 +1000

    Converted Groups.js into functional component

--- src/containers/content/identity/Groups.js
+++ src/containers/content/identity/Groups.js
@@ -16,8 +16,8 @@
  */
 
 import {connect} from "react-redux";
-import {LinearProgress, withStyles, withTheme} from "@material-ui/core";
-import React from "react";
+import {LinearProgress, makeStyles, withTheme} from "@material-ui/core";
+import React, {useEffect} from "react";
 import Avatar from "@material-ui/core/es/Avatar";
 import ListItemText from "@material-ui/core/es/ListItemText";
 import ListItem from "@material-ui/core/ListItem";
@@ -27,7 +27,7 @@ import List from "@material-ui/core/List";
 import EmptyCard from "../../../components/widget/EmptyCard";
 import Center from "react-center";
 import Pagination from "material-ui-flat-pagination/lib/Pagination";
-import {LS_SORT, pageSize} from "../../../constants";
+import {pageSize} from "../../../constants";
 import {getGroups, GROUP_LOAD} from "../../../actions/Groups";
 import Icon from "@mdi/react";
 import {mdiAccountGroupOutline} from "@mdi/js";
@@ -38,135 +38,116 @@ import {IconButton} from "@material-ui/core";
 import AddIcon from "@material-ui/icons/Add";
 import CreateGroupDialog from "../../modal/CreateGroupDialog";
 import {setGroupNew} from "../../../actions/Modal";
+import {setOffset, setSort} from "../../../actions/Generic";
 
 const Item = posed.div({
 	enter: {opacity: 1},
 	exit: {opacity: 0}
 });
 
-const styles = () => ({
+const useStyles = makeStyles(() => ({
 	title: {fontFamily: "Manrope", fontWeight: 500},
-	button: {
-		// margin: theme.spacing.unit,
-	},
 	grow: {flexGrow: 1}
-});
+}));
 
-class Groups extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			groups: props.groups,
-			offset: 0,
-			headers: props.headers,
-			searchFilter: props.searchFilter,
-			sort: localStorage.getItem(LS_SORT),
-			sorts: [
-				{id: 'name', value: "Name"},
-				{id: '-name', value: "Name Desc"},
-				{id: 'creation', value: "Creation"},
-				{id: 'updated', value: "Last edited"}
-			],
-			showCreateDialog: false,
-		};
-		this.filterGroup = this.filterGroup.bind(this);
-	}
-
-	componentWillReceiveProps(nextProps, nextContext) {
-		if(nextProps.headers !== this.state.headers) {
-			// Load jumps from the API
-			this.props.getGroups(nextProps.headers);
-		}
-		this.setState({...nextProps});
-	}
-
-	componentDidMount() {
-		this.props.getGroups(this.state.headers);
-	}
-	filterGroup(group) {
-		return group.name.toLowerCase().includes(this.state.searchFilter) ||
-			group.from.toLowerCase() === this.state.searchFilter.toLowerCase();
-	}
-	handlePageChange(offset) {
-		this.setState({offset: offset});
-	}
-	handleSortChange(e, value) {
-		this.setState({sort: value});
-		localStorage.setItem(LS_SORT, value);
-		this.props.getGroups(this.state.headers);
-	}
-	setCreateDialog = (visible) => {
-		this.setState({showCreateDialog: visible});
+const Groups = props => {
+	const sorts = [
+		{id: 'name', value: "Name"},
+		{id: '-name', value: "Name Desc"},
+		{id: 'creation', value: "Creation"},
+		{id: 'updated', value: "Last edited"}
+	];
+	
+	useEffect(() => {
+		props.getGroups(props.headers);
+	}, [props.headers]);
+	
+	const filterGroup = group => {
+		return group.name.toLowerCase().includes(props.searchFilter) ||
+			group.from.toLowerCase() === props.searchFilter.toLowerCase();
 	};
-	static capitalise(text) {
+	const handlePageChange = (offset) => {
+		props.setOffset(offset);
+	};
+	const handleSortChange = value => {
+		props.setSort(value);
+		props.getGroups(props.headers);
+	};
+	const capitalise = (text) => {
 		if(text == null || text.length === 0) return text;
 		if(text.toLowerCase() === "ldap") return "LDAP";
 		return text.substring(0, 1).toUpperCase() + text.substring(1, text.length).toLowerCase();
-	}
-	render() {
-		const {classes, theme} = this.props;
-		let listItems = [];
-		// Tell the loop what our pagination limits are
-		let max = (this.state.offset + pageSize);
-		if(max > this.state.groups.length) max = this.state.groups.length;
-		let sortedGroups = sortItems(this.state.groups, this.state.sort);
-		sortedGroups.filter(this.filterGroup).forEach((i, index) => {
-			if(index < this.state.offset || index > max) return;
-			let avatar = {
-				bg: theme.palette.info.light,
-				fg: theme.palette.info.dark
-			};
-			let secondary = <span>{Groups.capitalise(i.from)}</span>;
-			listItems.push((
-				<ListItem button disableRipple key={index} component={'li'}>
-					<Avatar component={'div'} style={{backgroundColor: avatar.bg, color: avatar.fg, marginRight: 12}}>
-						<Icon path={mdiAccountGroupOutline} size={1} color={avatar.fg}/>
-					</Avatar>
-					<ListItemText primary={<span className={classes.title}>{i.name}</span>} secondary={secondary}/>
-				</ListItem>
-			));
-		});
-		// TODO move to component
-		const subHeader = (<ListSubheader className={classes.title} inset component={"div"}>
-			Groups {this.state.searchFilter != null && this.state.searchFilter.length > 0 ? `(${listItems.length} results)` : ''}
-			{/*<div className={classes.grow}/>*/}
-			<SortButton selectedSort={this.state.sort} sorts={this.state.sorts} onSubmit={(e, value) => this.handleSortChange(e, value)}/>
-			<IconButton centerRipple={false} className={classes.button} aria-label="Add" onClick={() => this.props.setGroupNew(true)}><AddIcon fontSize={"small"}/></IconButton>
-			<CreateGroupDialog/>
-		</ListSubheader>);
+	};
+	const {theme} = props;
+	const classes = useStyles();
+	let listItems = [];
+	// Tell the loop what our pagination limits are
+	let max = (props.offset + pageSize);
+	if(max > props.groups.length) max = props.groups.length;
+	let sortedGroups = sortItems(props.groups, props.sort);
+	sortedGroups.filter(filterGroup).forEach((i, index) => {
+		if(index < props.offset || index > max) return;
+		let avatar = {
+			bg: theme.palette.info.light,
+			fg: theme.palette.info.dark
+		};
+		let secondary = <span>{capitalise(i.from)}</span>;
+		listItems.push((
+			<ListItem button disableRipple key={index} component={'li'}>
+				<Avatar component={'div'} style={{backgroundColor: avatar.bg, color: avatar.fg, marginRight: 12}}>
+					<Icon path={mdiAccountGroupOutline} size={1} color={avatar.fg}/>
+				</Avatar>
+				<ListItemText primary={<span className={classes.title}>{i.name}</span>} secondary={secondary}/>
+			</ListItem>
+		));
+	});
+	// TODO move to component
+	const subHeader = (<ListSubheader className={classes.title} inset component={"div"}>
+		Groups {props.searchFilter != null && props.searchFilter.length > 0 ? `(${listItems.length} results)` : ''}
+		{/*<div className={classes.grow}/>*/}
+		<SortButton selectedSort={props.sort} sorts={sorts} onSubmit={(e, value) => handleSortChange(value)}/>
+		<IconButton centerRipple={false} aria-label="Add" onClick={() => props.setGroupNew(true)}><AddIcon fontSize={"small"}/></IconButton>
+		<CreateGroupDialog/>
+	</ListSubheader>);
 
-		return (
-			<div>
-				{subHeader}
-				{this.state.loading === true ? <LinearProgress className={classes.grow} color={"primary"}/> : "" }
-				<PoseGroup animateOnMount={true}>
-					<Paper key={"root"} component={Item} style={{borderRadius: 12, marginBottom: 8}}>
-						<List component={'ul'}>
-							{listItems.length > 0 ? listItems : <EmptyCard/>}
-						</List>
-					</Paper>
-				</PoseGroup>
-				{listItems.length > pageSize || this.state.offset > 0 ?
-					<Center>
-						<Pagination limit={pageSize} offset={this.state.offset} total={sortedGroups.length}
-			                nextPageLabel={"▶"} previousPageLabel={"◀"} onClick={(e ,offset) => this.handlePageChange(offset)}/>
-					</Center>
-					:
-					<div/>
-				}
-			</div>
-		)
-	}
-}
+	return (
+		<div>
+			{subHeader}
+			{props.loading === true ? <LinearProgress className={classes.grow} color={"primary"}/> : "" }
+			<PoseGroup animateOnMount={true}>
+				<Paper key={"root"} component={Item} style={{borderRadius: 12, marginBottom: 8}}>
+					<List component={'ul'}>
+						{listItems.length > 0 ? listItems : <EmptyCard/>}
+					</List>
+				</Paper>
+			</PoseGroup>
+			{listItems.length > pageSize || props.offset > 0 ?
+				<Center>
+					<Pagination limit={pageSize} offset={props.offset} total={sortedGroups.length}
+		                nextPageLabel={"▶"} previousPageLabel={"◀"} onClick={(e ,offset) => handlePageChange(offset)}/>
+				</Center>
+				:
+				<div/>
+			}
+		</div>
+	)
+};
 
 const mapStateToProps = state => ({
-	groups: state.groups.groups || [],
+	groups: state.groups.groups,
 	loading: state.loading[GROUP_LOAD],
 	headers: state.auth.headers,
-	searchFilter: state.generic.searchFilter
+	sort: state.generic.sort,
+	searchFilter: state.generic.searchFilter,
+	offset: state.generic.offset
 });
 const mapDispatchToProps = ({
 	getGroups,
-	setGroupNew
+	setGroupNew,
+	setOffset,
+	setSort
 });
-export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(withTheme(Groups)));
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(withTheme(Groups));
