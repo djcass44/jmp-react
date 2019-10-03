9bec4efa7af91b896345cc662bf8eff7afe0e01d
Django Cass <dj.cass44@gmail.com>
2019-10-03 22:42:02 +1000

    Minor styling tweaks to the Login page

--- src/containers/content/Login.js
+++ src/containers/content/Login.js
@@ -16,16 +16,17 @@
  */
 
 import React, {useEffect, useState} from "react";
-import {Button, Card, CardContent, CircularProgress, Grid, makeStyles, TextField, Typography} from "@material-ui/core";
+import {Button, Card, CardContent, CircularProgress, Grid, makeStyles, Typography} from "@material-ui/core";
 import {OAUTH_REQUEST, oauthRequest} from "../../actions/Auth";
 import {useDispatch, useSelector} from "react-redux";
 import Center from "react-center";
-import {getVersion} from "../../actions/Generic";
 import SocialButton from "../../components/widget/SocialButton";
 import {mdiGithubCircle, mdiGoogle} from "@mdi/js";
 import {oauth2Discover} from "../../actions/Oauth";
 import {APP_NAME} from "../../constants";
 import {useTheme} from "@material-ui/core/styles";
+import ValidatedTextField from "../../components/field/ValidatedTextField";
+import getIconColour from "../../style/getIconColour";
 
 const useStyles = makeStyles(theme => ({
 	title: {
@@ -58,11 +59,12 @@ const initialPassword = {
 };
 
 export default ({history}) => {
-	// props hooks
+	// hooks
 	const dispatch = useDispatch();
-	const providers = useSelector(state => state.auth.providers);
-	const {isLoggedIn} = useSelector(state => state.auth);
-	const {version} = useSelector(state => state.generic);
+	const classes = useStyles();
+	const theme = useTheme();
+
+	const {isLoggedIn, providers} = useSelector(state => state.auth);
 	const loading = useSelector(state => state.loading[OAUTH_REQUEST]);
 	const error = useSelector(state => state.errors[OAUTH_REQUEST]);
 
@@ -72,7 +74,6 @@ export default ({history}) => {
 	// lifecycle hooks
 	useEffect(() => {
 		window.document.title = `Login - ${APP_NAME}`;
-		getVersion(dispatch);
 		oauth2Discover(dispatch, "github");
 		oauth2Discover(dispatch, "google");
 	}, [dispatch]);
@@ -85,28 +86,17 @@ export default ({history}) => {
 			history.push(target != null && target !== "" ? target : "/");
 		}
 	}, [isLoggedIn]);
-	const onUsernameChange = (e) => {
-		const {value} = e.target;
-		const err = username.regex.test(value) === true ? "" : "Username must be a minimum of 3 characters";
-		setUsername({...username, value, err});
-	};
-	const onPasswordChange = (e) => {
-		const {value} = e.target;
-		const err = password.regex.test(value) === true ? "" : "Password must be a minimum of 8 characters";
-		setPassword({...password, value, err});
-	};
+
 	const onSubmit = () => {
 		const data = window.btoa(`${username.value}:${password.value}`);
 		oauthRequest(dispatch, data);
 	};
-	const classes = useStyles();
-	const theme = useTheme();
 	let errorMessage = <div/>;
 	if(error != null) {
 		errorMessage = (
 			<Center>
 				<span style={{color: theme.palette.error.main}}>
-					{error.toString().startsWith("Unauthorized") ? "Incorrect username or password" : error.toString()}
+					{(error.toString().startsWith("Unauthorized") || error.toString().includes("status code 404")) ? "Incorrect username or password" : error.toString()}
 				</span>
 			</Center>
 		);
@@ -117,47 +107,79 @@ export default ({history}) => {
 				<CircularProgress/>
 				:
 				<>
-					<Grid container spacing={4} alignContent={"center"} justify={"center"}>
+					<Grid container spacing={4} alignContent="center" justify="center">
 						<Grid item lg={2} md={false}/>
 						<Grid item lg={8} md={12}>
 							<CardContent style={{margin: 12}}>
-								<Grid container spacing={4} alignContent={"center"} justify={"center"}>
+								<Grid container spacing={4} alignContent="center" justify="center">
 									<Grid item xs={12}>
-										<Center><img src={`${process.env.PUBLIC_URL}/jmp.png`} alt={"App icon"} height={72}/></Center>
-										<Typography className={classes.banner} variant={"h2"} align={"center"}>{APP_NAME}</Typography>
+										<Center>
+											<img src={`${process.env.PUBLIC_URL}/jmp.png`} alt="App icon" height={72}/>
+										</Center>
+										<Typography className={classes.banner} variant="h2"
+										            align="center">{APP_NAME}</Typography>
 									</Grid>
 									<Grid item xs={12}>
 										<Card style={{padding: 16}}>
-											<TextField required autoFocus autoComplete={"username"} margin={"dense"} id={"username"} label={"Username"} variant={"outlined"}
-											           value={username.value} fullWidth error={username.error.length !== 0} helperText={username.error}
-											           onChange={(e) => onUsernameChange(e)}/>
-											<TextField required type={"password"} autoComplete={"password"} margin={"dense"} id={"password"} label={"Password"} variant={"outlined"}
-											           value={password.value} fullWidth error={password.error.length !== 0} helperText={password.error}
-											           onChange={(e) => onPasswordChange(e)}/>
-											<Button style={{marginTop: 8}} className={classes.title} onClick={() => onSubmit()} variant={"contained"} color={"primary"}
-											        fullWidth size={"large"} type={"submit"} disabled={loading === true || username.error !== '' ||
+											<ValidatedTextField
+												data={username}
+												setData={setUsername}
+												invalidLabel="Username must be a minimum of 3 characters"
+												fieldProps={{
+													required: true,
+													autoFocus: true,
+													autoComplete: "username",
+													margin: "dense",
+													id: "username",
+													label: "Username",
+													variant: "outlined",
+													fullWidth: true
+												}}
+											/>
+											<ValidatedTextField
+												data={password}
+												setData={setPassword}
+												invalidLabel="Password must be a minimum of 8 characters"
+												fieldProps={{
+													required: true,
+													autoComplete: "password",
+													margin: "dense",
+													id: "password",
+													label: "Password",
+													variant: "outlined",
+													fullWidth: true
+												}}
+											/>
+											<Button style={{
+												marginTop: 8,
+												textTransform: "none",
+												color: getIconColour(theme)
+											}} className={classes.title} onClick={() => onSubmit()} variant="contained"
+											        color="primary"
+											        fullWidth size="large" type="submit"
+											        disabled={loading === true || username.error !== '' ||
 													password.error !== '' || username.value.length === 0 || password.value.length === 0}>
-												Login
+												Sign in
 											</Button>
 										</Card>
 									</Grid>
 								</Grid>
-								<Center className={classes.title} style={{padding: 8}}>{APP_NAME}&nbsp;{version}</Center>
-								{errorMessage}
-								{Object.keys(providers).length > 0 ?
+								{/*<Center className={classes.title} style={{padding: 8}}>{APP_NAME}&nbsp;{version}</Center>*/}
+								<Typography style={{padding: 8}}>
+									{errorMessage}
+								</Typography>
+								{Object.keys(providers).length > 0 &&
 									<>
-										<p className={classes.oauthMessage}>Login with</p>
+										<p className={classes.oauthMessage}>Alternatively, login with</p>
 										<Center>
-											{providers['github'] === true ?
+											{providers['github'] === true &&
 												<SocialButton id={"github"} name={"GitHub"} colour={"#171516"}
-												              icon={mdiGithubCircle}/> : ""}
-											{providers['google'] === true ?
+												              icon={mdiGithubCircle}/>}
+											{providers['google'] === true &&
 												<SocialButton id={"google"} name={"Google"} colour={"#4285F4"}
-												              icon={mdiGoogle}/> : ""}
+												              icon={mdiGoogle}/>}
 										</Center>
 									</>
-									:
-									""
 								}
 							</CardContent>
 						</Grid>
