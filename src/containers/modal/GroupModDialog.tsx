a380d42f8f44405f03e77e6888c89705e970de7e
Django Cass <django@dcas.dev>
2020-04-27 14:00:10 +1000

    Converted CreateGroup,GroupMod dialog to TypeScript

--- src/containers/modal/GroupModDialog.tsx
+++ src/containers/modal/GroupModDialog.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {ReactNode, useEffect, useState} from "react";
 import {
 	Button,
 	Checkbox,
@@ -16,15 +16,17 @@ import {
 	Typography
 } from "@material-ui/core";
 import {useDispatch, useSelector} from "react-redux";
-import {sortItems} from "../../misc/Sort";
 import Center from "react-center";
 import {MODAL_USER_GROUPS, setDialog} from "../../store/actions/Modal";
-import {defaultState} from "../../store/reducers/modal";
+import {defaultState, Modal} from "../../store/reducers/modal";
 import {clone} from "../../util";
 import {GROUP_LOAD} from "../../store/actions/groups/GetGroups";
 import {GET_USER_GROUPS, getUserGroups} from "../../store/actions/groups/GetUserGroups";
 import {SET_USER_GROUPS, setUserGroups} from "../../store/actions/groups/SetUserGroups";
 import useAuth from "../../hooks/useAuth";
+import {TState} from "../../store/reducers";
+import {GroupsState} from "../../store/reducers/groups";
+import {Group} from "../../types";
 
 const useStyles = makeStyles(() => ({
 	title: {
@@ -34,15 +36,23 @@ const useStyles = makeStyles(() => ({
 	},
 	button: {
 		fontFamily: "Manrope",
-		fontWeight: 'bold'
+		fontWeight: "bold"
 	},
 }));
 
 class GroupModPayload {
-	constructor(add, rm) {
+	private add: Array<string>;
+	private rm: Array<string>;
+
+	constructor(add: Array<string>, rm: Array<string>) {
 		this.add = add;
 		this.rm = rm;
 	}
+}
+
+interface UserMap {
+	group: Group;
+	checked: boolean;
 }
 
 export default () => {
@@ -51,30 +61,38 @@ export default () => {
 	const dispatch = useDispatch();
 
 	const {headers} = useAuth();
-	const {groups, userGroups} = useSelector(state => state.groups);
-	const loading = useSelector(state => state.loading[GROUP_LOAD] || state.loading[GET_USER_GROUPS] || state.loading[SET_USER_GROUPS]);
-	const {other, open} = useSelector(state => state.modal[MODAL_USER_GROUPS] || defaultState);
+	const {groups, userGroups} = useSelector<TState, GroupsState>(state => state.groups);
+	const loading = useSelector<TState, boolean>(state => state.loading[GROUP_LOAD] || state.loading[GET_USER_GROUPS]);
+	const loadingChange = useSelector<TState, boolean>(state => state.loading[SET_USER_GROUPS]);
+	const {other, open} = useSelector<TState, Modal>(state => state.modal[MODAL_USER_GROUPS] || defaultState);
 
 	const user = other?.user || {};
 
 
-	const [usermap, setUsermap] = useState([]);
-	const [items, setItems] = useState([]);
+	const [usermap, setUsermap] = useState<Array<UserMap>>([]);
+	const [items, setItems] = useState<Array<ReactNode>>([]);
 
 	const updateGroupMappings = () => {
-		let mapping = [];
+		const mapping: Array<UserMap> = [];
 		// Check whether the user is in each group and build a mapping array
 		groups.content.forEach(g => {
-			const g2 = clone(g);
-			g2.checked = userGroups.some(e => e.name === g.name);
-			mapping.push(g2);
+			mapping.push({
+				group: clone(g),
+				checked: userGroups.some(e => e.name === g.name)
+			});
 		});
 		setUsermap(mapping);
 	};
 	useEffect(() => {
-		if (open === true)
-			updateGroupMappings()
+		if (open)
+			updateGroupMappings();
 	}, [groups, userGroups]);
+
+	useEffect(() => {
+		if (loadingChange || user == null)
+			return;
+		getUserGroups(dispatch, headers, user.id);
+	}, [loadingChange]);
 
 	useEffect(() => {
 		/**
@@ -82,36 +100,40 @@ export default () => {
 		 * @param index: position of the group in the userMap
 		 * @param checked: whether the group should be added or removed
 		 */
-		const onChange = (index, checked) => {
+		const onChange = (index: number, checked: boolean): void => {
 			const item = usermap[index];
 			let add = [];
 			let rm = [];
-			if (checked === true)
-				add.push(item.id);
+			if (checked)
+				add.push(item.group.id);
 			else
-				rm.push(item.id);
+				rm.push(item.group.id);
 			const payload = new GroupModPayload(add, rm);
-			item.loading = true;
 			setUserGroups(dispatch, headers, user.id, JSON.stringify(payload));
 		};
 
-		const onChecked = index => {
+		const onChecked = (index: number): void => {
 			onChange(index, !usermap[index].checked);
 		};
 
-		const sortedGroups = sortItems(usermap, 'name');
-		setItems(sortedGroups.map((i, index) => {
+		setItems(usermap.map((i, index) => {
+			const {group} = i;
 			return (
-				<ListItem key={i.id} component='li' role={undefined} dense>
+				<ListItem key={group.id} component='li' role={undefined} dense>
 					<ListItemIcon>
-						<Checkbox color="primary" edge="start" checked={i.checked === true}
-						          disabled={loading === true || i.loading === true || i.public === true || i.name.startsWith("_")}
-						          tabIndex={-1}
-						          onChange={() => onChecked(index)}/>
+						<Checkbox
+							color="primary"
+							edge="start"
+							checked={i.checked}
+							disabled={loading || loadingChange || group.public || group.name.startsWith("_")}
+							tabIndex={-1}
+							onChange={() => onChecked(index)}
+						/>
 					</ListItemIcon>
-					<ListItemText id={i.id} primary={i.name}/>
-					{loading === true || i.loading === true ?
-						<ListItemSecondaryAction><CircularProgress size={15}/></ListItemSecondaryAction> : ""}
+					<ListItemText id={group.id} primary={group.name}/>
+					{loading && <ListItemSecondaryAction>
+						<CircularProgress size={15}/>
+					</ListItemSecondaryAction>}
 				</ListItem>
 			)
 		}));
@@ -124,7 +146,7 @@ export default () => {
 	};
 
 	return (
-		<Dialog open={open === true} aria-labelledby="form-dialog-title" onEnter={() => onOpen()}>
+		<Dialog open={open} aria-labelledby="form-dialog-title" onEnter={() => onOpen()}>
 			<DialogTitle id="form-dialog-title">
 				<Typography className={classes.title}>
 					Modify groups
@@ -132,10 +154,10 @@ export default () => {
 			</DialogTitle>
 			<DialogContent>
 				<Typography variant="body1">
-					Here you can modify the groups that {user?.displayName || user?.username || 'the user'} is in.
+					Here you can modify the groups that {user?.displayName || user?.username || "the user"} is in.
 				</Typography>
 				<div style={{margin: 12}}>
-					{loading === true && usermap.length === 0 ?
+					{loading && usermap.length === 0 ?
 						<Center><CircularProgress/></Center>
 						:
 						<List component='ul'>
@@ -145,9 +167,13 @@ export default () => {
 				</div>
 			</DialogContent>
 			<DialogActions>
-				<Button className={classes.button} color="secondary"
-				        onClick={() => setDialog(dispatch, MODAL_USER_GROUPS, false, null)}
-				        disabled={loading === true}>Done</Button>
+				<Button
+					className={classes.button}
+					color="secondary"
+					onClick={() => setDialog(dispatch, MODAL_USER_GROUPS, false, null)}
+					disabled={loading}>
+					Done
+				</Button>
 			</DialogActions>
 		</Dialog>
 	);
